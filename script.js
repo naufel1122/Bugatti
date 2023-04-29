5c4b71f467066d83421c4813e1eda73165330f18
naufel1122 <nofil.khan037@gmail.com>
2023-04-29 16:20:26 +0500

    index complete almost and working on centodeci

---
+++ script.js
@@ -0,0 +1,29 @@
+// THEME JAVASCRIPT
+const bodyEl = document.body
+			const switchEl = document.querySelector('.switch')
+			const activeTheme = localStorage.getItem('theme')
+
+			if (activeTheme) {
+				bodyEl.classList.add('dark')
+			}
+            switchEl.addEventListener('click', function () {
+				bodyEl.classList.toggle('dark')
+				if (bodyEl.classList.contains('dark')) {
+					localStorage.setItem('theme', 'dark')
+				} else {
+					localStorage.removeItem('theme')
+				}
+			})
+			
+// THEME JAVASCRIPT ENDS
+
+
+
+			// NAVIGATION SCROLLING JAVASCRIPT START
+const isScrolling = () => {
+    const headerEl = document.querySelector('.primary-header')
+    let windowPosition = window.scrollY > 250
+    headerEl.classList.toggle('active', windowPosition)
+  }
+  window.addEventListener('scroll', isScrolling);
+			// NAVIGATION SCROLLING JAVASCRIPT START
