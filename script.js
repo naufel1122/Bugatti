f18d1d145094785fc2b431ce5ca80167f0354cf7
naufel1122 <nofil.khan037@gmail.com>
2023-05-02 23:50:57 +0500

    centodeici update

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 // THEME JAVASCRIPT
-const bodyEl = document.body
-			const switchEl = document.querySelector('.switch__ball')
-			const activeTheme = localStorage.getItem('theme')
+const bodyEl = document.body;
+const switchEl = document.querySelector(".switch__ball");
+const activeTheme = localStorage.getItem("theme");
 
-			if (activeTheme) {
-				bodyEl.classList.add('dark')
-			}
-            switchEl.addEventListener('click', function () {
-				bodyEl.classList.toggle('dark')
-				if (bodyEl.classList.contains('dark')) {
-					localStorage.setItem('theme', 'dark')
-				} else {
-					localStorage.removeItem('theme')
-				}
-			})
-			
+if (activeTheme) {
+  bodyEl.classList.add("dark");
+}
+switchEl.addEventListener("click", function () {
+  bodyEl.classList.toggle("dark");
+  if (bodyEl.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.removeItem("theme");
+  }
+});
+
 // THEME JAVASCRIPT ENDS
 
-
-
-			// NAVIGATION SCROLLING JAVASCRIPT START
+// NAVIGATION SCROLLING JAVASCRIPT START
 const isScrolling = () => {
-    const headerEl = document.querySelector('.primary-header')
-    let windowPosition = window.scrollY > 350
-    headerEl.classList.toggle('active', windowPosition)
-  }
-  window.addEventListener('scroll', isScrolling);
-			// NAVIGATION SCROLLING JAVASCRIPT START
+  const headerEl = document.querySelector(".primary-header");
+  let windowPosition = window.scrollY > 350;
+  headerEl.classList.toggle("active", windowPosition);
+};
+window.addEventListener("scroll", isScrolling);
+// NAVIGATION SCROLLING JAVASCRIPT START
+
